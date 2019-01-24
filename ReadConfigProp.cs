5c07197a2366237d4fed48695775bc3af7835eb7
sarelpoy <30390238+sarelpoy@users.noreply.github.com>
2019-01-24 12:47:39 +0200

    Update ReadConfigProp.cs

--- ReadConfigProp.cs
+++ ReadConfigProp.cs
@@ -1,14 +1,17 @@
- public static T GetAppParam<T>(string configParam, ConversionFunction<T> conversionFunction ,T defaultValue)
+class AppConfigParam
 {
-     T returnValue = defaultValue;
-     string fromConfig = ConfigurationSettings.AppSettings[configParam]; ;
-     try
-     {
-         returnValue = conversionFunction(fromConfig);
-     }
-     catch(Exception e)
-     {
-         Console.WriteLine(e.Message);
-     }
-     return returnValue;
+ public static T GetAppParam<T>(string configParam, ConversionFunction<T> conversionFunction ,T defaultValue)
+ {
+      T returnValue = defaultValue;
+      string fromConfig = ConfigurationSettings.AppSettings[configParam]; ;
+      try
+      {
+          returnValue = conversionFunction(fromConfig);
+      }
+      catch(Exception e)
+      {
+          Console.WriteLine(e.Message);
+      }
+      return returnValue;
+ }
 }
