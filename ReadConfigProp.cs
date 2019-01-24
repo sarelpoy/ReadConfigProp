6d15d55dfb02d36d12054def7010ae659ed9bd0c
sarelpoy <30390238+sarelpoy@users.noreply.github.com>
2019-01-24 12:43:02 +0200

    Create ReadConfigProp.cs

---
+++ ReadConfigProp.cs
@@ -0,0 +1,14 @@
+ public static T GetAppParam<T>(string configParam, ConversionFunction<T> conversionFunction ,T defaultValue)
+{
+     T returnValue = defaultValue;
+     string fromConfig = ConfigurationSettings.AppSettings[configParam]; ;
+     try
+     {
+         returnValue = conversionFunction(fromConfig);
+     }
+     catch(Exception e)
+     {
+         Console.WriteLine(e.Message);
+     }
+     return returnValue;
+}
